a1ee0f781e02fec5eba7b4af421d0eb0889b86bb
Prasoon2050 <76505015+Prasoon2050@users.noreply.github.com>
2023-12-06 22:32:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BookingPage from "./BookingPage";
+import BookingSystem from "./BookingSystem";
+import Header from "./Header";
+import Profile from "./Profile";
+import AppointmentType from "./AppointmentType";
+import FAQ from "./FAQ";
+import "./App.css";
+
+const Mainpage = () => (
+  <div className="bg-gray-100" style={{ paddingTop: "60px" }}>
+    <Header />
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "70%", backgroundColor: "white" }}>
+        <Profile />
+        <BookingPage />
+        <FAQ />
+      </div>
+    </div>
+  </div>
+);
+
+const Book = () => (
+  <div className="bg-gray-100" style={{ paddingTop: "60px" }}>
+    <Header />
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "70%", backgroundColor: "white" }}>
+        <Profile />
+        <BookingSystem />
+        <FAQ />
+      </div>
+    </div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/booking/:date/:time/:type" element={<Book />} />
+      </Routes>
+    </Router>
   );
 }
 
